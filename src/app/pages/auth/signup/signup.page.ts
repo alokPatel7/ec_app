e7eed938aa335dbb5cb48f3d41fb0d0e968e070f
alok_patel <alokpatel863@gmail.com>
2023-08-16 20:29:28 +0530

    add validation for signup

--- src/app/pages/auth/signup/signup.page.ts
+++ src/app/pages/auth/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  signupForm: any;
+  passwordRegex = /^(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-])(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.signupForm = this.formBuilder.group({
+      fullName: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]],
+      confirmPassword: ['', [Validators.required]],
+    }, {
+      validators: this.passwordMatchValidator
+    });
+  }
 
   ngOnInit() {
+  }
+
+  submitForm() {
+    if (this.signupForm.valid) {
+      // Process the form data
+      console.log('@ form data', this.signupForm.value);
+
+    }
+  }
+
+  passwordMatchValidator(formGroup: FormGroup) {
+    const password = formGroup?.get('password')?.value;
+    const confirmPassword = formGroup?.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      formGroup?.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+    } else {
+      formGroup?.get('confirmPassword')?.setErrors(null); // Corrected line
+    }
   }
 
 }
